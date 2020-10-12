4441768aab732b6541175fc3922e94c603c28a5a
Marko <marko@email.com>
2020-10-12 19:56:45 +0200

    created page

---
+++ posts.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'header.php';
+include_once 'footer.php';
+include_once 'sidebar.php';
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../../../favicon.ico">
+
+        <title>Vivify Blog</title>
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+        <!-- Custom styles for this template -->
+        <link href="styles/blog.css" rel="stylesheet">
+    </head>
+    <body>
+    
+    </body>
+</html>
